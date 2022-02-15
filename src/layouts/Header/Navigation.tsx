a7b773b226e772f0b61bef23735db73bafc7d929
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-02-15 19:45:21 +0800

    fix(theme): using black bg instead of zinc, adding id

--- src/layouts/Header/Navigation.tsx
+++ src/layouts/Header/Navigation.tsx
@@ -1,6 +1,6 @@
 export default function Navigation() {
   return (
-    <nav class="flex items-center justify-between">
+    <nav class="flex items-center justify-between" id="navigation">
       <a>Become Instructor</a>
       <a>For Business</a>
       <a>Log In</a>
--- src/layouts/Header/Search.tsx
+++ src/layouts/Header/Search.tsx
@@ -1,12 +1,16 @@
 export default function Search() {
   return (
     <div class="flex justify-center items-center">
-      <fieldset class="bg-zinc-800 w-max rounded-full flex py-1.5 px-1.5 text-sm gap-2 shadow-lg">
+      <fieldset class="dark:bg-zinc-900 w-max rounded-full flex py-1.5 px-1.5 text-sm gap-2 shadow-lg">
         <input
-          class="bg-transparent pl-2 placeholder:text-zinc-500 text-sm"
+          class="bg-transparent pl-2 placeholder:text-zinc-500 text-sm leading-none"
           placeholder="Start Your Search"
+          id="search-input"
         ></input>
-        <button class="flex bg-red-500 rounded-full w-6 h-6 justify-center items-center">
+        <button
+          id="search-submit-button"
+          class="flex bg-red-500 rounded-full w-6 h-6 justify-center items-center"
+        >
           <span class="material-icons-outlined !text-base">search</span>
         </button>
       </fieldset>
