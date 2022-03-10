52dcc325ee454b71145aaac50c27cdbcf6c3aa91
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-03-10 11:48:30 +0800

    fix(style): apply dark style by default, removing `dark:`

--- src/layouts/Header/Search.tsx
+++ src/layouts/Header/Search.tsx
@@ -1,7 +1,7 @@
 export default function Search() {
   return (
     <div className="hidden lg:flex justify-center items-center">
-      <fieldset className="dark:bg-zinc-900 w-max rounded-full flex py-1.5 px-1.5 text-sm gap-2 shadow-lg">
+      <fieldset className="bg-zinc-900 w-max rounded-full flex py-1.5 px-1.5 text-sm gap-2 shadow-lg">
         <input
           className="bg-transparent w-52 pl-2 placeholder:text-zinc-500 text-sm leading-none"
           placeholder="Start Your Search"
