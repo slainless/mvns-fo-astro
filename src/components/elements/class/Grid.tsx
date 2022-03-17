cdd4032c2f09e01b495a2c8ab35da803587906cf
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-03-17 17:37:04 +0800

    `@Elements/class/Grid`: use dummy data

--- src/components/elements/class/Grid.tsx
+++ src/components/elements/class/Grid.tsx
@@ -1,84 +1,11 @@
 import { Common as Card } from '@Blocks/Card'
 import cntl from 'cntl'
-
-const Items: Parameters<typeof Card>[0][] = [
-  {
-    bgImg: '/media/trending-card-2.jpg',
-    title: 'Developing Entrepreneurial Mindset',
-    // subtitle: 'Chai Li',
-    badges: ['Live', 'Finance & Accounting'],
-    favorite: true,
-    price: '49.65',
-    quota: '8/10',
-    date: 'Jan 21-29, 22',
-  },
-  {
-    bgImg: '/media/trending-card-2.jpg',
-    title: 'Developing Entrepreneurial Mindset',
-    // subtitle: 'Kevin Sanjaya',
-    badges: ['Offline', 'IT & Software'],
-    favorite: true,
-    price: '49.65',
-    quota: '8/10',
-    date: 'Jan 21-29, 22',
-  },
-  {
-    bgImg: '/media/trending-card-2.jpg',
-    title: 'Developing Entrepreneurial Mindset',
-    // subtitle: 'Richard Dawkins',
-    favorite: false,
-    badges: ['Recording', 'Business'],
-    price: '49.65',
-  },
-  {
-    bgImg: '/media/trending-card-2.jpg',
-    title: 'Developing Entrepreneurial Mindset',
-    // subtitle: 'Richard Dawkins',
-    badges: ['Recording', 'Health & Fitness'],
-    favorite: true,
-    price: '49.65',
-  },
-  {
-    bgImg: '/media/trending-card-2.jpg',
-    title: 'Developing Entrepreneurial Mindset',
-    // subtitle: 'Chai Li',
-    badges: ['Live', 'Finance & Accounting'],
-    favorite: true,
-    price: '49.65',
-    quota: '8/10',
-    date: 'Jan 21-29, 22',
-  },
-  {
-    bgImg: '/media/trending-card-2.jpg',
-    title: 'Developing Entrepreneurial Mindset',
-    // subtitle: 'Kevin Sanjaya',
-    badges: ['Offline', 'IT & Software'],
-    favorite: true,
-    price: '49.65',
-    quota: '8/10',
-    date: 'Jan 21-29, 22',
-  },
-  {
-    bgImg: '/media/trending-card-2.jpg',
-    title: 'Developing Entrepreneurial Mindset',
-    // subtitle: 'Richard Dawkins',
-    badges: ['Recording', 'Business'],
-    price: '49.65',
-  },
-  {
-    bgImg: '/media/trending-card-2.jpg',
-    title: 'Developing Entrepreneurial Mindset',
-    // subtitle: 'Richard Dawkins',
-    badges: ['Recording', 'Health & Fitness'],
-    favorite: true,
-    price: '49.65',
-  },
-]
+import { slimCard as Items } from '@Dev/dummy'
 
 export default function Grid() {
   return (
     <div id="classes-grid" className="grid grid-cols-4 gap-x-4 gap-y-8">
-      {Items.map((item, key) => (
+      {Items.slice(0, 8).map((item, key) => (
         <Card
           key={key}
           {...item}
