3904731d5c3cd1aa7b32ce95dae1555ea7f8994a
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-03-29 06:13:03 +0800

    feat(page): basic sitemap

---
+++ src/components/elements/sitemap/List.tsx
@@ -0,0 +1,59 @@
+import { Link } from '@Bits/Button'
+import cntl from 'cntl'
+import { twMerge } from 'tailwind-merge'
+type Props = HTMLAttr<'div'> & {
+  items: [display: string, href: string][]
+  title: string
+  styleOverrides?: {
+    container?: string
+    title?: string
+    list?: {
+      style?: string
+      item?: string
+    }
+  }
+}
+
+const ItemStyle = cntl`
+  tracking-normal transition-colors
+  text-white/50 before:h-[2px] before:bg-white/50 before:w-full
+  before:absolute before:left-0 before:-bottom-1 hover:before:w-full
+  before:transition-all
+
+  after:w-0 hover:text-white hover:before:bg-white
+`
+export default function List(props: Props) {
+  const { className, styleOverrides, items, title, ...rest } = props
+
+  return (
+    <div
+      className={twMerge(
+        'list flex flex-col gap-8',
+        className,
+        styleOverrides?.container
+      )}
+      {...rest}
+    >
+      <h2 className={twMerge('font-heading text-2xl', styleOverrides?.title)}>
+        {title}
+      </h2>
+      <ul
+        className={twMerge(
+          'grid grid-cols-2 gap-y-3 max-w-3xl',
+          styleOverrides?.list?.style
+        )}
+      >
+        {items.map((item, key) => (
+          <li key={item[0] + key}>
+            <Link
+              href={item[1]}
+              className={twMerge(ItemStyle, styleOverrides?.list?.item)}
+            >
+              {item[0]}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
---
+++ src/components/elements/sitemap/Sitemap.tsx
@@ -0,0 +1,42 @@
+import Section from '@Blocks/Section'
+import List from './List'
+
+export default function Sitemap() {
+  return (
+    <Section.Container>
+      <Section.Title className="lg:text-4xl">Sitemap</Section.Title>
+      <Section.Content className="flex flex-col gap-12 lg:mt-12">
+        <List
+          title="Pages"
+          items={[
+            ['Home', '/'],
+            ['Contact Us', '/contact-us'],
+            ['Help Center', '/help'],
+            ['Privacy & Policy', '/privacy'],
+            ['Careers', '/career'],
+            ['Terms', '/terms'],
+            ['For Business', '/business'],
+            ['All Classes', '/class/all'],
+            ['Become Instructor', '/instructor'],
+            ['Blog', '/blog/all'],
+          ]}
+        />
+        <List
+          title="Class Categories"
+          items={[
+            ['Design', '/class/all'],
+            ['Personal Development Us', '/class/all'],
+            ['IT & Software', '/class/all'],
+            ['Business & Policy', '/class/all'],
+            ['Marketing', '/class/all'],
+            ['Photography & Video', '/class/all'],
+            ['Teaching & Academics', '/class/all'],
+            ['Health & Fitness', '/help'],
+            ['Finance & Accounting', '/privacy'],
+          ]}
+        />
+        <List title="Blog" items={[['Blog', '/blog/all']]} />
+      </Section.Content>
+    </Section.Container>
+  )
+}
