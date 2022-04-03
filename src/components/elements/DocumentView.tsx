33f301a1fc94b3689c1740a0e51b706d1569a841
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-04-03 15:25:25 +0800

    `@Elements/DocumentView`: use `@Blocks/Section`

--- src/components/elements/DocumentView.tsx
+++ src/components/elements/DocumentView.tsx
@@ -4,15 +4,18 @@ import { twMerge } from 'tailwind-merge'
 import * as Dialog from '@radix-ui/react-dialog'
 import Navigation from './DocumentView/Navigation'
 import AltNavigation from './DocumentView/AltNavigation'
+import Section from '@Blocks/Section'
 
 type Props = HTMLAttr<'div'> & {
-  hideAds?: boolean
+  title?: string
   styleOverrides?: {
     container?: string
+    content?: string
+    title?: string
   }
 }
 export default function DocumentView(props: Props) {
-  const { children, className, hideAds, styleOverrides, ...rest } = props
+  const { children, className, title, styleOverrides, ...rest } = props
   const [headings, setHeadings] = useState<HTMLHeadingElement[]>([])
   const [activeHeading, setActiveHeading] = useState<HTMLHeadingElement[]>([])
 
@@ -70,29 +73,31 @@ export default function DocumentView(props: Props) {
   }, [])
 
   return (
-    <section
-      id="document"
-      className={twMerge('flex', styleOverrides?.container)}
-    >
-      <main className="flex flex-col md:flex-row px-5 xs:px-7 sm:px-16 md:gap-16 lg:gap-20">
-        <Navigation
-          className="md:w-48 lg:w-64 pr-8 md:h-[calc(100vh-theme(spacing.header)-theme(spacing.20))]"
-          headings={headings}
-          activeHeadings={activeHeading}
-        />
-        <AltNavigation headings={headings} activeHeadings={activeHeading} />
-        <article
-          id="main-article"
-          className={twMerge(
-            'flex-grow max-w-4xl prose prose-invert',
-            'prose-h2:before:w-14 prose-h2:before:bg-red-600 prose-h2:before:h-2 first:prose-h2:mt-0',
-            'prose-h2:relative prose-h2:before:absolute prose-h2:before:-top-3',
-            className
-          )}
-        >
-          {children}
-        </article>
-      </main>
-    </section>
+    <Section.Container className={twMerge(styleOverrides?.container)}>
+      <Section.Title className={twMerge(styleOverrides?.title)}>
+        {title}
+      </Section.Title>
+      <Section.Content className={twMerge(styleOverrides?.content)}>
+        <main className="flex flex-col md:flex-row md:gap-16 lg:gap-20">
+          <Navigation
+            className="md:w-48 lg:w-64 pr-8 md:h-[calc(100vh-theme(spacing.header)-theme(spacing.20))]"
+            headings={headings}
+            activeHeadings={activeHeading}
+          />
+          <AltNavigation headings={headings} activeHeadings={activeHeading} />
+          <article
+            id="main-article"
+            className={twMerge(
+              'flex-grow max-w-4xl prose prose-invert',
+              'prose-h2:before:w-14 prose-h2:before:bg-red-600 prose-h2:before:h-2 first:prose-h2:mt-0',
+              'prose-h2:relative prose-h2:before:absolute prose-h2:before:-top-3',
+              className
+            )}
+          >
+            {children}
+          </article>
+        </main>
+      </Section.Content>
+    </Section.Container>
   )
 }
