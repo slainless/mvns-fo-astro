657674278a4810312caa3846b89fdcf7833aa504
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-04-09 22:11:45 +0800

    feat(page): :iphone: responsiveness for checkout page

--- src/components/elements/cart/Checkout.tsx
+++ src/components/elements/cart/Checkout.tsx
@@ -12,8 +12,11 @@ export default function Checkout() {
     <Section.Container>
       <Section.Title>Checkout</Section.Title>
       <Section.Content className="lg:mt-12">
-        <form id="checkout-form" className="grid grid-cols-2 gap-x-10 gap-y-5">
-          <div className="flex flex-col gap-5">
+        <form
+          id="checkout-form"
+          className="flex flex-col sm:grid grid-cols-2 lg:gap-x-10 gap-x-5 gap-y-5 sm:gap-y-10"
+        >
+          <div className="flex flex-col gap-y-5">
             <h2 className="font-heading text-lg">Payment Method</h2>
             <Input label="Credit Card" name="credit-card" />
             <Input
@@ -21,12 +24,12 @@ export default function Checkout() {
               name="cc-number"
               leadingIcon="credit_card"
             />
-            <div className="flex flex-row gap-10">
+            <div className="flex flex-col xs:flex-row sm:flex-col lg:flex-row gap-y-5 gap-x-5 sm:gap-x-2 md:gap-x-10">
               <fieldset className="flex flex-col gap-2">
                 <legend className="mb-2 uppercase text-xs tracking-widest text-white/80">
                   Expiration Date
                 </legend>
-                <div className="flex flex-row gap-5">
+                <div className="flex flex-row gap-5 sm:gap-x-2 md:gap-x-5">
                   <Input name="month" placeholder="Month" />
                   <Input name="year" placeholder="Year" />
                 </div>
@@ -40,7 +43,7 @@ export default function Checkout() {
             </div>
             <Input label="Visa" name="visa" />
           </div>
-          <div className="grid grid-cols-2 gap-x-5 gap-y-5">
+          <div className="grid grid-cols-2 gap-x-5 sm:gap-x-2 md:gap-x-5 gap-y-5">
             <h2 className="font-heading text-lg col-span-2">Billing Details</h2>
             <Input label="First Name" leadingIcon="person" />
             <Input label="Last Name" />
@@ -65,7 +68,7 @@ export default function Checkout() {
             id="cart-summary"
             className="bg-zinc-900 rounded-lg p-10 w-full col-span-2 mt-10 mb-5 flex flex-col gap-8"
           >
-            <h2 className="font-heading text-2xl">
+            <h2 className="font-heading text-lg sm:text-2xl">
               <Icon icon="shopping_cart" className="flex gap-3">
                 Cart Summary
               </Icon>
@@ -74,12 +77,14 @@ export default function Checkout() {
               {cartItems.map((item, key) => (
                 <li
                   key={(item.product?.name ?? '') + key}
-                  className="flex justify-between py-5 border-b-2 border-white/10"
+                  className="flex flex-col sm:flex-row sm:justify-between gap-x-5 gap-y-2 py-5 border-b-2 border-white/10"
                 >
                   <div className="product">
                     {item.quantity ?? 1} x {item.product?.name}
                   </div>
-                  <div className="price">{item.price}</div>
+                  <div className="price tracking-wider self-end">
+                    ${item.price}
+                  </div>
                 </li>
               ))}
             </ul>
@@ -94,9 +99,9 @@ export default function Checkout() {
               <Checkbox
                 name="agree"
                 id="agree"
-                className="border-white/30 hover:border-red-600 transition-colors"
+                className="border-white/30 hover:border-red-600 transition-colors ring-offset-2 ring-offset-black"
                 styleOverrides={{
-                  indicator: cntl`bg-red-600 border-red-600`,
+                  indicator: cntl`bg-red-600 border-red-600 ring-offset-2 ring-offset-black`,
                 }}
               />
               <Label htmlFor="agree">
