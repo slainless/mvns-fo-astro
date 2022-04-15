0d4d4ae3c8731d5de237e29c870d6be9a1eb155e
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-04-15 16:14:03 +0800

    feat(element): :sparkles: `@Elements/CategoryBar` overhaul
    * progress on issue #39
    
    * added responsivity
    
    * DRY-refactored
    
    * added lightweight dynamic edge style using `IntersectionObserver`
    
    * simplified scrolling logic, now using half of the container's width

--- src/components/elements/CategoryBar.tsx
+++ src/components/elements/CategoryBar.tsx
@@ -1,9 +1,11 @@
 import { Arrow, Common as Button, Icon } from '@Bits/Button'
 import { getScrollVisibilityState } from '@Functions/dom-helper'
 import isBrowser from '@Functions/isBrowser'
-import { useCallback, useRef } from 'react'
+import cntl from 'cntl'
+import { useCallback, useRef, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
 import './CategoryBar.styl'
+import { useEffect } from 'react'
 
 const categories = [
   ['Design'],
@@ -17,104 +19,120 @@ const categories = [
   ['Finance & Accounting'],
 ]
 
-function getHidden(el: HTMLElement, reverse = false) {
-  const THRESHOLD = 85
-  const STEP = 3
-
-  const children = Array.from(el.children) as HTMLElement[]
-  const length = children.length
-
-  const getPrev = (i: number) => (reverse ? i + 1 : i - 1)
-  const getStep = () => (reverse ? STEP - 1 : -(STEP - 1))
-  const get = (i: number): number | null => {
-    const state = getScrollVisibilityState(children[i], true)
-    if (state?.x == 'visible') return getPrev(i) + getStep()
-    else if (state?.x == 'partial-left' || state?.x == 'partial-right') {
-      if (state.percentage.x < THRESHOLD) return i + getStep()
-      return getPrev(i) + getStep()
-    }
-    return null
-  }
-
-  for (
-    let i = reverse ? length - 1 : 0;
-    reverse ? i >= 0 : i < length;
-    reverse ? i-- : i++
-  ) {
-    const gotIndex = get(i)
-    if (gotIndex == null) continue
-    const normalizedIndex =
-      gotIndex < 0 ? 0 : gotIndex >= length ? length - 1 : gotIndex
-    const el = children[normalizedIndex]
-    if (el instanceof HTMLElement)
-      return [el, getScrollVisibilityState(el)!, normalizedIndex] as const
-  }
-  return null
+type NavButtonProps = Parameters<typeof Arrow>[0] & {
+  containerClass?: string
 }
-
-if (isBrowser) {
-  // @ts-ignore
-  window['test'] = getScrollVisibilityState
+function NavButton(props: NavButtonProps) {
+  const { className, containerClass, arrowType, ...rest } = props
+  return (
+    <div
+      className={twMerge(
+        arrowType == 'prev'
+          ? 'pr-3 left-0 after:bg-gradient-to-r'
+          : 'pl-3 right-0 after:bg-gradient-to-l after:order-first',
+        'flex flex-row absolute h-full',
+        'after:w-5 after:from-black after:to-transparent after:h-full',
+        containerClass
+      )}
+    >
+      <div
+        className={twMerge(
+          arrowType == 'prev' ? 'pr-3' : 'pl-3',
+          'hidden lg:flex place-items-center bg-black h-full'
+        )}
+      >
+        <Arrow
+          arrowType={arrowType}
+          className={twMerge(
+            'w-7 h-7 focus:ring-2 ring-offset-2 ring-offset-black text-base',
+            className
+          )}
+          {...rest}
+        ></Arrow>
+      </div>
+    </div>
+  )
 }
 
 type Props = HTMLAttr<'div'>
 export default function CategoryBar(props: Props) {
   const { className, ...rest } = props
   const ref = useRef<HTMLDivElement>(null)
+  const [[leftEdgeVisible, rightEdgeVisible], setEdgeVisibility] = useState<
+    [boolean, boolean]
+  >([false, false])
 
-  const next = useCallback(() => {
-    const target = ref.current
-    if (target == null) return
-    const hiddenEl = getHidden(target, true)
-    console.log(hiddenEl)
-    if (hiddenEl == null) return
-    const [scrollTarget, scrollState, i] = hiddenEl
-    target.scrollBy({
-      left: scrollState.pos.element.right - scrollState.pos.view.right + 5,
-      behavior: 'smooth',
-    })
+  useEffect(() => {
+    const container = ref.current
+    if (container == null) return
+
+    const children = Array.from(container.children)
+    if (children.length === 0) return
+    const leftEdge = children.shift()
+    const rightEdge = children.pop()
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        for (const entry of entries) {
+          const { target } = entry
+          console.log(entries)
+          if (target === leftEdge)
+            setEdgeVisibility((old) => [entry.intersectionRatio > 0.9, old[1]])
+          if (target === rightEdge)
+            setEdgeVisibility((old) => [old[0], entry.intersectionRatio > 0.9])
+        }
+      },
+      {
+        root: ref.current,
+        threshold: 0.9,
+      }
+    )
+
+    if (leftEdge) observer.observe(leftEdge)
+    if (rightEdge) observer.observe(rightEdge)
   }, [ref])
 
-  const prev = useCallback(() => {
-    const target = ref.current
-    if (target == null) return
-    const hiddenEl = getHidden(target)
-    if (hiddenEl == null) return
-    const [scrollTarget, scrollState, i] = hiddenEl
-    console.log(scrollTarget, scrollState)
-    target.scrollBy({
-      left: scrollState.pos.element.left - scrollState.pos.view.left - 5,
-      behavior: 'smooth',
-    })
-  }, [ref])
+  const scrollFn = useCallback(
+    (direction: Parameters<typeof Arrow>[0]['arrowType']) => () => {
+      const bar = ref.current
+      if (bar == null) return
+      const scrollValue = bar.getClientRects()[0].width / 2
+      bar.scrollBy({
+        left: direction == 'prev' ? -scrollValue : scrollValue,
+        // behavior: 'smooth',
+      })
+    },
+    [ref]
+  )
+
   return (
     // hacks to make container rigid
     <div className={twMerge('grid relative', className)} {...rest}>
-      <Arrow
-        onClick={prev}
-        arrowType="prev"
-        className="absolute w-7 h-7 text-base top-1/2 hover:ring-2 focus:ring-2 ring-red-600/50 -translate-y-1/2 left-0"
-      />
-      <Arrow
-        onClick={next}
-        arrowType="next"
-        className="absolute w-7 h-7 text-base top-1/2 hover:ring-2 focus:ring-2 ring-red-600/50 -translate-y-1/2 right-0"
-      />
       <div
         id="category-filter"
-        className="flex flex-1 gap-2 overflow-auto mx-10 relative"
+        className="flex flex-1 gap-2 overflow-auto relative scroll-smooth snap-x snap-proximity"
         ref={ref}
       >
         {categories.map((category) => (
           <Button
             key={category[0]}
             mods={['no-translate', 'no-fill', 'to-fill-red']}
-            className="shrink-0 text-white rounded-full normal-case text-sm py-1 px-4"
+            className="shrink-0 text-white rounded-full normal-case text-sm py-1 px-4 snap-start"
           >
             {category[0]}
           </Button>
         ))}
       </div>
+      <NavButton
+        containerClass={leftEdgeVisible ? 'hidden' : ''}
+        arrowType="prev"
+        onClick={scrollFn('prev')}
+      />
+      <NavButton
+        containerClass={rightEdgeVisible ? 'hidden' : ''}
+        arrowType="next"
+        onClick={scrollFn('next')}
+      />
     </div>
   )
 }
