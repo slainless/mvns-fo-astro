62af9f28f80c911d8df7d3f5ea204560c603fa36
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-04-18 16:02:59 +0800

    feat(page): :construction: basic responsiveness for class grid view
    TODO: mobile view for popup elements

--- src/components/elements/class/Grid.tsx
+++ src/components/elements/class/Grid.tsx
@@ -4,14 +4,17 @@ import { slimCard as Items } from '@Dev/dummy'
 
 export default function Grid() {
   return (
-    <div id="classes-grid" className="grid grid-cols-4 gap-x-4 gap-y-8">
+    <div
+      id="classes-grid"
+      className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-8"
+    >
       {Items.slice(0, 8).map((item, key) => (
         <Card
           key={key}
           {...item}
           styleOverrides={{
             card: {
-              container: cntl`h-[28rem] overflow-visible`,
+              container: cntl`h-[18rem] sm:h-[24rem] md:h-[24rem] lg:h-[28rem] overflow-visible`,
               overlay: cntl`scale-100 group-hover:scale-[1.05] transition-all rounded-md`,
               background: cntl`scale-100 group-hover:scale-[1.05] transition-all rounded-md`,
             },
