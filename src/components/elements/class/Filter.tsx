d46f8d6067fc3faeface9689db4d96474f5218d5
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-04-18 16:01:27 +0800

    feat(element): filter bar ui update
    progress on issue #39
    
    * now using `@Elements/CarouselBar` to render filter buttons
    
    * added responsivity

--- src/components/elements/class/Filter.tsx
+++ src/components/elements/class/Filter.tsx
@@ -7,39 +7,28 @@ import Rating from './Filter/Rating'
 import { Common as Swiper } from '@Blocks/Carousel'
 import { SwiperSlide } from 'swiper/react'
 import CategoryBar from '@Elements/CategoryBar'
-
-const Tags = [
-  'Finance & Accounting',
-  'IT & Software',
-  'Personal Development',
-  'Health & Fitness',
-  'Politic & Government',
-]
+import CarouselBar from '@Elements/CarouselBar'
 
 export default function Filter() {
   return (
-    <div id="filter" className="flex items-center gap-5">
-      <div id="filter-list" className="flex gap-2 shrink-0">
+    <div
+      id="filter"
+      className="flex flex-col md:flex-row md:items-center gap-x-5 gap-y-0"
+    >
+      <CarouselBar
+        disableNav={true}
+        className="shrink-0 order-last md:order-first"
+      >
         <DateDialog />
         <Pricing />
         <Popular />
         <Rating />
-      </div>
+      </CarouselBar>
       <Separator
         orientation="vertical"
-        className="h-5 w-[1px] bg-white/50 shrink-0"
+        className="hidden md:block h-5 w-[1px] bg-white/50 shrink-0"
       />
-      <CategoryBar />
-      {/* <div id="tag-list" className="flex flex-row gap-5 flex-grow">
-        <div id="tag-active" className="w-max shrink-0">
-          <span className="underline underline-offset-4 w-max">
-            <span className="material-icons-outlined underline">
-              work_outline
-            </span>{' '}
-            Business
-          </span>
-        </div>
-      </div> */}
+      <CategoryBar className="order-first md:order-last" />
     </div>
   )
 }
--- src/components/elements/class/Filter/Button.tsx
+++ src/components/elements/class/Filter/Button.tsx
@@ -11,9 +11,9 @@ const Button = forwardRef(
         ref={ref}
         {...rest}
         className={twMerge(
-          'text-sm py-1 font-heading border-2 border-white',
+          'text-sm py-1 font-heading border-2 border-white flex shrink-0',
           'bg-white text-black rounded-full px-3',
-          'hover:bg-transparent hover:shadow-lg hover:shadow-white/30 hover:text-white transition-all duration-300',
+          'hover:bg-transparent hover:shadow-md hover:shadow-white/30 hover:text-white transition-all duration-300',
           className
         )}
       >
