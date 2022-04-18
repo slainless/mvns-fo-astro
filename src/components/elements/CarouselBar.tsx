0f54f30f13a480a4f99116fe5f5775939a425c78
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-04-18 15:56:06 +0800

    refactor(element): `@Elements/CarouselBar`
    progress on issue #39
    
    * moved logic from `@Elements/CategoryBar` to `@Elements/CarouselBar`
    
    * renamed style to match file
    
    * style using postcss instead
    
    * element now using class instead of id to allow reusability
    
    * changed initial state to `[true, false]`
    
    * hide scrollbar on smaller screen (< lg)

---
+++ src/components/elements/CarouselBar.tsx
@@ -0,0 +1,151 @@
+import { Arrow, Common as Button, Icon } from '@Bits/Button'
+import cntl from 'cntl'
+import { useCallback, useRef, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+import './CarouselBar.css'
+import { useEffect } from 'react'
+
+type NavButtonProps = Parameters<typeof Arrow>[0] & {
+  containerClass?: string
+  disableNav?: boolean
+}
+function NavButton(props: NavButtonProps) {
+  const { className, containerClass, arrowType, disableNav, ...rest } = props
+  return (
+    <div
+      className={twMerge(
+        arrowType == 'prev'
+          ? 'pr-3 left-0 after:bg-gradient-to-r'
+          : 'pl-3 right-0 after:bg-gradient-to-l after:order-first',
+        'flex flex-row absolute h-full',
+        'after:w-5 after:from-black after:to-transparent after:h-full',
+        containerClass
+      )}
+    >
+      {disableNav ? (
+        <></>
+      ) : (
+        <div
+          className={twMerge(
+            arrowType == 'prev' ? 'pr-3' : 'pl-3',
+            'hidden lg:flex place-items-center bg-black h-full'
+          )}
+        >
+          <Arrow
+            arrowType={arrowType}
+            className={twMerge(
+              'w-7 h-7 focus:ring-2 ring-offset-2 ring-offset-black text-base',
+              className
+            )}
+            {...rest}
+          ></Arrow>
+        </div>
+      )}
+    </div>
+  )
+}
+
+type Props = HTMLAttr<'div'> & {
+  disableNav?: boolean
+  defaultVisible?: boolean
+  styleOverrides?: {
+    container?: string
+    bar?: string
+  }
+}
+export default function CarouselBar(props: Props) {
+  const {
+    className,
+    children,
+    styleOverrides,
+    disableNav,
+    defaultVisible,
+    ...rest
+  } = props
+  const ref = useRef<HTMLDivElement>(null)
+  const [[leftEdgeVisible, rightEdgeVisible], setEdgeVisibility] = useState<
+    [boolean, boolean]
+  >(defaultVisible ? [true, true] : [true, false])
+
+  useEffect(() => {
+    const THRESHOLD = 0.99
+    const container = ref.current
+    if (container == null) return
+
+    const children = Array.from(container.children)
+    if (children.length === 0) return
+    const leftEdge = children.shift()
+    const rightEdge = children.pop()
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        for (const entry of entries) {
+          const { target } = entry
+          if (target === leftEdge)
+            setEdgeVisibility((old) => [
+              entry.intersectionRatio > THRESHOLD,
+              old[1],
+            ])
+          if (target === rightEdge)
+            setEdgeVisibility((old) => [
+              old[0],
+              entry.intersectionRatio > THRESHOLD,
+            ])
+        }
+      },
+      {
+        root: ref.current,
+        threshold: THRESHOLD,
+      }
+    )
+
+    if (leftEdge) observer.observe(leftEdge)
+    if (rightEdge) observer.observe(rightEdge)
+  }, [ref])
+
+  const scrollFn = useCallback(
+    disableNav
+      ? () => () => {}
+      : (direction: Parameters<typeof Arrow>[0]['arrowType']) => () => {
+          const bar = ref.current
+          if (bar == null) return
+          const scrollValue = bar.getClientRects()[0].width / 2
+          bar.scrollBy({
+            left: direction == 'prev' ? -scrollValue : scrollValue,
+            // behavior: 'smooth',
+          })
+        },
+    [ref]
+  )
+
+  return (
+    // hacks to make container rigid
+    <div
+      className={twMerge('grid relative', className, styleOverrides?.container)}
+      {...rest}
+    >
+      <div
+        className={twMerge(
+          'carousel-bar',
+          'flex flex-1 gap-2 overflow-auto scroll-smooth',
+          styleOverrides?.bar
+        )}
+        ref={ref}
+      >
+        {children}
+      </div>
+      <NavButton
+        disableNav={disableNav}
+        containerClass={leftEdgeVisible ? 'hidden' : ''}
+        arrowType="prev"
+        onClick={scrollFn('prev')}
+      />
+      <NavButton
+        disableNav={disableNav}
+        containerClass={rightEdgeVisible ? 'hidden' : ''}
+        arrowType="next"
+        onClick={scrollFn('next')}
+      />
+    </div>
+  )
+}
--- src/components/elements/CategoryBar.tsx
+++ src/components/elements/CategoryBar.tsx
@@ -1,11 +1,5 @@
 import { Arrow, Common as Button, Icon } from '@Bits/Button'
-import { getScrollVisibilityState } from '@Functions/dom-helper'
-import isBrowser from '@Functions/isBrowser'
-import cntl from 'cntl'
-import { useCallback, useRef, useState } from 'react'
-import { twMerge } from 'tailwind-merge'
-import './CategoryBar.styl'
-import { useEffect } from 'react'
+import CarouselBar from './CarouselBar'
 
 const categories = [
   ['Design'],
@@ -19,120 +13,20 @@ const categories = [
   ['Finance & Accounting'],
 ]
 
-type NavButtonProps = Parameters<typeof Arrow>[0] & {
-  containerClass?: string
-}
-function NavButton(props: NavButtonProps) {
-  const { className, containerClass, arrowType, ...rest } = props
-  return (
-    <div
-      className={twMerge(
-        arrowType == 'prev'
-          ? 'pr-3 left-0 after:bg-gradient-to-r'
-          : 'pl-3 right-0 after:bg-gradient-to-l after:order-first',
-        'flex flex-row absolute h-full',
-        'after:w-5 after:from-black after:to-transparent after:h-full',
-        containerClass
-      )}
-    >
-      <div
-        className={twMerge(
-          arrowType == 'prev' ? 'pr-3' : 'pl-3',
-          'hidden lg:flex place-items-center bg-black h-full'
-        )}
-      >
-        <Arrow
-          arrowType={arrowType}
-          className={twMerge(
-            'w-7 h-7 focus:ring-2 ring-offset-2 ring-offset-black text-base',
-            className
-          )}
-          {...rest}
-        ></Arrow>
-      </div>
-    </div>
-  )
-}
-
-type Props = HTMLAttr<'div'>
+type Props = Omit<Parameters<typeof CarouselBar>[0], 'children'>
 export default function CategoryBar(props: Props) {
-  const { className, ...rest } = props
-  const ref = useRef<HTMLDivElement>(null)
-  const [[leftEdgeVisible, rightEdgeVisible], setEdgeVisibility] = useState<
-    [boolean, boolean]
-  >([false, false])
-
-  useEffect(() => {
-    const container = ref.current
-    if (container == null) return
-
-    const children = Array.from(container.children)
-    if (children.length === 0) return
-    const leftEdge = children.shift()
-    const rightEdge = children.pop()
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        for (const entry of entries) {
-          const { target } = entry
-          console.log(entries)
-          if (target === leftEdge)
-            setEdgeVisibility((old) => [entry.intersectionRatio > 0.9, old[1]])
-          if (target === rightEdge)
-            setEdgeVisibility((old) => [old[0], entry.intersectionRatio > 0.9])
-        }
-      },
-      {
-        root: ref.current,
-        threshold: 0.9,
-      }
-    )
-
-    if (leftEdge) observer.observe(leftEdge)
-    if (rightEdge) observer.observe(rightEdge)
-  }, [ref])
-
-  const scrollFn = useCallback(
-    (direction: Parameters<typeof Arrow>[0]['arrowType']) => () => {
-      const bar = ref.current
-      if (bar == null) return
-      const scrollValue = bar.getClientRects()[0].width / 2
-      bar.scrollBy({
-        left: direction == 'prev' ? -scrollValue : scrollValue,
-        // behavior: 'smooth',
-      })
-    },
-    [ref]
-  )
-
+  const { ...rest } = props
   return (
-    // hacks to make container rigid
-    <div className={twMerge('grid relative', className)} {...rest}>
-      <div
-        id="category-filter"
-        className="flex flex-1 gap-2 overflow-auto relative scroll-smooth snap-x snap-proximity"
-        ref={ref}
-      >
-        {categories.map((category) => (
-          <Button
-            key={category[0]}
-            mods={['no-translate', 'no-fill', 'to-fill-red']}
-            className="shrink-0 text-white rounded-full normal-case text-sm py-1 px-4 snap-start"
-          >
-            {category[0]}
-          </Button>
-        ))}
-      </div>
-      <NavButton
-        containerClass={leftEdgeVisible ? 'hidden' : ''}
-        arrowType="prev"
-        onClick={scrollFn('prev')}
-      />
-      <NavButton
-        containerClass={rightEdgeVisible ? 'hidden' : ''}
-        arrowType="next"
-        onClick={scrollFn('next')}
-      />
-    </div>
+    <CarouselBar {...rest}>
+      {categories.map((category) => (
+        <Button
+          key={category[0]}
+          mods={['no-translate', 'no-fill', 'to-fill-red']}
+          className="shrink-0 text-white rounded-full normal-case text-sm py-1 px-4 snap-start"
+        >
+          {category[0]}
+        </Button>
+      ))}
+    </CarouselBar>
   )
 }
