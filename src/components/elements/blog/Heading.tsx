0e7ae81d5f1f2ea985adb89ede95ae261b88fcdf
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2022-05-14 07:48:02 +0800

    added skeleton loaders to blog

--- src/components/elements/blog/Heading.tsx
+++ src/components/elements/blog/Heading.tsx
@@ -2,11 +2,14 @@ import BlogAPI, { useBlogStore } from '@Api/blog'
 import { Icon } from '@Bits/Button'
 import { Blog, BlogResponse } from '@Class/blog'
 import Share from '@Elements/Share'
+import isBrowser from '@Functions/isBrowser'
 import { AspectRatio } from '@radix-ui/react-aspect-ratio'
 import { Separator } from '@radix-ui/react-separator'
 import { useRequest } from 'ahooks'
 import { nanoid } from 'nanoid'
 import { useEffect, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
+import shallow from 'zustand/shallow'
 
 type Props = {
   title: string
@@ -18,7 +21,10 @@ type Props = {
 }
 
 export default function Heading() {
-  const article = useBlogStore((state) => state.article)
+  const [article, loading] = useBlogStore(
+    (state) => [state.article, state.loading],
+    shallow
+  )
 
   // const article = {
   //   title: 'Everything You Need to Know About Business & Marketing',
@@ -34,17 +40,36 @@ export default function Heading() {
       <header id="blog-heading" className="flex flex-col gap-2">
         <h1
           id="blog-title"
-          className="text-2xl xs:text-3xl sm:text-4xl font-heading max-w-[30ch] font-bold order-2"
+          className={twMerge(
+            'order-2',
+            !isBrowser || loading
+              ? 'h-10 w-[32rem] skeleton-zinc-light'
+              : 'text-2xl xs:text-3xl sm:text-4xl font-heading max-w-[30ch] font-bold'
+          )}
         >
-          {article?.title ??
-            'Everything You Need to Know About Business & Marketing'}
+          {article?.title}
         </h1>
         <div className="order-3">
-          <div id="blog-author" className="text-red-600 font-bold">
+          <div
+            id="blog-author"
+            className={twMerge(
+              !isBrowser || loading
+                ? 'h-6 w-32 skeleton-zinc-light'
+                : 'text-red-600 font-bold'
+            )}
+          >
             {/* {article?.user_id} */}
             {'Paul Bankhead'}
           </div>
-          <div id="blog-date" className="text-black/50 text-sm">
+          <div
+            id="blog-date"
+            className={twMerge(
+              'mt-1',
+              !isBrowser || loading
+                ? 'h-6 w-32 skeleton-zinc-light'
+                : 'text-black/50 text-sm'
+            )}
+          >
             {/* {article?.updated_at} */}
             {'January 20, 2022'}
           </div>
@@ -64,9 +89,13 @@ export default function Heading() {
         <AspectRatio ratio={16 / 9} className="relative">
           <div
             id="blog-image"
-            className="w-full h-full bg-cover"
+            className={twMerge(
+              'w-full h-full bg-cover',
+              !isBrowser || loading ? 'skeleton-zinc-light' : ''
+            )}
             style={{
-              backgroundImage: `url('https://picsum.photos/800')`,
+              backgroundImage:
+                !isBrowser || loading ? '' : `url('https://picsum.photos/800')`,
             }}
           ></div>
           <Share
@@ -82,13 +111,23 @@ export default function Heading() {
           >
             <Icon
               icon="share"
-              className="absolute bottom-0 left-0 flex flex-row gap-2 py-2 px-4 bg-black/50 text-white text-base transition-colors hover:bg-red-600/50"
+              className={twMerge(
+                'absolute bottom-0 left-0 flex flex-row gap-2 py-2 px-4 bg-black/50 text-white text-base transition-colors hover:bg-red-600/50',
+                !isBrowser || loading ? 'hidden' : ''
+              )}
             >
               Share
             </Icon>
           </Share>
         </AspectRatio>
-        <div id="blog-image-caption" className="text-gray-500 text-sm">
+        <div
+          id="blog-image-caption"
+          className={twMerge(
+            !isBrowser || loading
+              ? 'w-64 h-5 skeleton-zinc-light'
+              : 'text-gray-500 text-sm'
+          )}
+        >
           {article?.subtitle}
         </div>
         <Separator className="w-full h-[2px] bg-gray-300" />
